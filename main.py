c01cbf0fd014fb9540a6d87c287a012399102515
Viktor Dick <vd@perfact.de>
2022-02-02 21:20:48 +0100

    React to resizes

--- main.py
+++ main.py
@@ -1,69 +1,102 @@
 #!/usr/bin/python
 
 from math import log
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import threading
 
-np.warnings.filterwarnings('ignore')
 
-def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
-    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
-    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
-    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
-
-xmin = -2
-xmax = 0.5
-ymin = -1.5
-ymax = 1.5
 ndata = 1024
 max_iter = 50
-esc_radius = 10 
+esc_radius = 20
+np.warnings.filterwarnings('ignore')
+
+
+def complex_matrix(xmin, xmax, ymin, ymax):
+    re = np.linspace(xmin, xmax, ndata, dtype=np.longdouble)
+    im = np.linspace(ymin, ymax, ndata, dtype=np.longdouble)
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+
 
 class Mandelbrot(threading.Thread):
-    def __init__(self):
-        self.iteration = 0
-        self.finished = False
-        self.data = np.zeros((ndata, ndata))
+    def __init__(self, xmin, xmax, ymin, ymax):
+        self.draw_version = 0
+        self.running = True
+        self.rect = (xmin, xmax, ymin, ymax)
+        self.data = None
         super().__init__()
 
     def run(self):
-        c = complex_matrix(xmin, xmax, ymin, ymax, ndata)
-        z = np.zeros_like(c, dtype=float)
-        result = np.ones_like(z)
-        diverged = np.zeros_like(c, dtype=bool)
+        rect = None
+        deviation = 100
 
-        while self.iteration < max_iter:
-            print(self.iteration)
+        while self.running:
+            if rect != self.rect:
+                rect = self.rect
+                print('resize:', rect)
+                c = complex_matrix(*rect)
+                z = np.zeros_like(c, dtype=np.longdouble)
+                result = np.ones_like(z)
+                diverged = np.zeros_like(c, dtype=bool)
+                iteration = 0
+                deviation = 100
+
+            if iteration >= max_iter:
+                time.sleep(1)
+                continue
+
+            print(self.draw_version, deviation)
             z = z ** 2 + c
             diverged_new = (abs(z) > esc_radius)
             np.copyto(
                 result,
-                (self.iteration + 1 - np.log(np.log(abs(z)))/log(2)) / max_iter,
+                (iteration + 1 - np.log(np.log(abs(z)))/log(2)) / max_iter,
                 where=diverged_new,
             )
             np.copyto(diverged, True, where=diverged_new)
             np.copyto(z, 2, where=diverged)
-
+            if self.data is not None:
+                deviation = np.linalg.norm(self.data - result)
             self.data = result.copy()
-            self.iteration += 1
+            iteration += 1
+            self.draw_version += 1
 
-        self.finished = True
 
-mandelbrot = Mandelbrot()
+mandelbrot = Mandelbrot(-2, -0.5, -1.5, 1.5)
 mandelbrot.start()
 
 plt.ion()
 plt.show()
+
+
+def on_zoom(ax):
+    mandelbrot.rect = ax.get_xlim() + ax.get_ylim()
+
+
+def on_close(event):
+    mandelbrot.running = False
+
+
+fig, ax = plt.subplots()
+c1 = ax.callbacks.connect('xlim_changed', on_zoom)
+c2 = ax.callbacks.connect('ylim_changed', on_zoom)
+fig.canvas.mpl_connect('close_event', on_close)
+
 drawn = 0
-while True:
-    if mandelbrot.iteration > drawn:
-        drawn = mandelbrot.iteration
-        print('Drawing', drawn)
-        plt.imshow(mandelbrot.data, 'Greys', extent=(xmin, xmax, ymin, ymax))
-    elif mandelbrot.finished:
-        break
-    plt.pause(1)
+while mandelbrot.running:
+    delay = 5
+    if mandelbrot.draw_version > drawn:
+        delay = 1
+        drawn = mandelbrot.draw_version
+        print('Draw', drawn)
+        plt.imshow(
+            mandelbrot.data,
+            'Greys',
+            extent=mandelbrot.rect,
+            origin='lower',
+        )
+    plt.pause(delay)
 
 mandelbrot.join()
 
