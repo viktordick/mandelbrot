b6016de334ad9012f02b8349d5fb0115f1570664
Viktor Dick <vd@perfact.de>
2023-10-05 13:29:42 +0200

    Add zooming

--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,32 @@ const SIZE: usize = WIDTH*ROWS;
 
 struct Grid {
     idx: usize,
-    step: usize,
+    anchor: Complex<f64>,
     eps: f64,
+    step: usize,
     c: Vec<Complex<f64>>,
     z: Vec<Complex<f64>>,
     diverged: BitVec,
     pixels: Vec<u8>,
-    zoom_hist: Vec<(f64, Complex<f64>)>,
 }
 
 impl Grid {
-    fn new(idx: usize, c: Complex<f64>, eps: f64) -> Grid {
+    fn new(idx: usize, anchor: Complex<f64>, eps: f64) -> Grid {
         let zero = Complex::new(0.0, 0.0);
-        let mut grid = Grid {
+        Grid {
             idx: idx,
+            anchor: anchor,
             step: 0,
             eps: eps,
             c: vec![zero; SIZE],
             z: vec![zero; SIZE],
             diverged: bitvec![0; SIZE],
             pixels: vec![0; 4*SIZE],
-            zoom_hist: Vec::new(),
-        };
-        grid.init(c + Complex::new(0.0, eps*(idx*ROWS) as f64));
-        grid
+        }
     }
 
-    fn init(&mut self, c: Complex<f64>) {
+    fn init(&mut self) {
+        let c = self.anchor + Complex::new(0.0, self.eps*(self.idx * ROWS) as f64);
         self.step = 0;
         for i in 0..ROWS {
             for j in 0..WIDTH {
@@ -65,22 +64,6 @@ impl Grid {
         for i in 0..4*SIZE {
             self.pixels[i] = 0;
         }
-        println!("{} {}", self.eps, self.c[1].re - self.c[0].re);
-    }
-
-    fn zoom_in(&mut self, corner: (usize, usize)) {
-        self.zoom_hist.push((self.eps, self.c[0]));
-        self.eps /= 2.0;
-        self.init(self.c[corner.1 * WIDTH + corner.0]);
-    }
-
-    fn zoom_out(&mut self) {
-        let (eps, c) = match self.zoom_hist.pop() {
-            None => return,
-            Some(x) => x,
-        };
-        self.eps = eps;
-        self.init(c);
     }
 
     fn update(&mut self) {
@@ -106,16 +89,23 @@ impl Grid {
     }
 }
 
-fn work(rcv: Receiver<Option<Grid>>, snd: Sender<Grid>) {
+enum Task {
+    Update(Grid),
+    Init(Grid),
+    Terminate,
+}
+
+fn work(rcv: Receiver<Task>, snd: Sender<Grid>) {
     loop {
-        let mut grid = match rcv.recv() {
-            Ok(x) => match x {
-                Some(grid) => grid,
-                None => break,
-            },
+        let task = match rcv.recv() {
+            Ok(task) => task,
             Err(_) => break,
         };
-        grid.update();
+        let grid = match task {
+            Task::Terminate => break,
+            Task::Update(mut grid) => {grid.update(); grid},
+            Task::Init(mut grid) => {grid.init(); grid},
+        };
         match snd.send(grid) {
             Ok(_) => (),
             Err(_) => break,
@@ -155,14 +145,14 @@ pub fn main() -> Result<(), String> {
     }
     thread::spawn(move || { work(rcv_thrd, snd_thrd); });
 
-    let mut grids = Vec::new();
+    let anchor = Complex::new(-3.0, -1.5);
+    let eps = 4.0/WIDTH as f64;
     for i in 0..NTHREADS {
-        grids.push(Grid::new(
-            i,
-            Complex::new(-3.0, -1.5),
-            4.0/WIDTH as f64,
-        ));
+        snd_main.send(Task::Init(Grid::new(i, anchor, eps))).map_err(|e| e.to_string())?;
     };
+
+    let mut zoom_hist = Vec::new();
+    let mut zoom = (anchor, eps);
 
     let mut corner = (0usize, 0usize);
     'running: loop {
@@ -179,31 +169,46 @@ pub fn main() -> Result<(), String> {
                     corner.0 = max(0, min(x - w / 4, w / 2)) as usize /16*16;
                     corner.1 = max(0, min(y - h / 4, h / 2)) as usize /16*16;
                 },
-                //Event::MouseButtonDown {..} => {
-                //    grid.zoom_in(corner);
-                //},
-                //Event::KeyDown { keycode: Some(Keycode::Backspace), .. } => {
-                //    grid.zoom_out();
-                //}
+                Event::MouseButtonDown {..} => {
+                    // Zoom in
+                    zoom_hist.push(zoom);
+                    let eps = zoom.1;
+                    let anchor = zoom.0 + Complex::new(corner.0 as f64, corner.1 as f64) * eps;
+                    zoom = (anchor, eps/2.0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Backspace), .. } => {
+                    // Zoom out
+                    if let Some(x) = zoom_hist.pop() {
+                        zoom = x;
+                    };
+                }
                 _ => continue,
             }
         }
-        while let Ok(grid) = rcv_main.try_recv() {
+        let mut received = 0;
+        while received < NTHREADS{
+            let mut grid = match rcv_main.try_recv() {
+                Err(_) => break,
+                Ok(grid) => grid,
+            };
+            received += 1;
             texture.update(
                 Rect::new(0, (grid.idx*ROWS) as i32, WIDTH as u32, ROWS as u32),
                 &grid.pixels,
                 4*WIDTH,
             ).map_err(|e| e.to_string())?;
-            grids.push(grid);
+            let task = if (grid.anchor, grid.eps) != zoom {
+                grid.anchor = zoom.0;
+                grid.eps = zoom.1;
+                Task::Init(grid)
+            } else {
+                Task::Update(grid)
+            };
+            snd_main.send(task).map_err(|e| e.to_string())?;
         };
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
-        if grids.len() == NTHREADS {
-            canvas.copy(&texture, None, None)?;
-            while let Some(grid) = grids.pop() {
-                snd_main.send(Some(grid)).map_err(|e| e.to_string())?;
-            };
-        }
+        canvas.copy(&texture, None, None)?;
         canvas.rectangle(
             corner.0 as i16,
             corner.1 as i16,
@@ -213,6 +218,9 @@ pub fn main() -> Result<(), String> {
         )?;
         canvas.present();
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    };
+    for _ in 0..NTHREADS {
+        snd_main.send(Task::Terminate).unwrap();
     };
 
     Ok(())
